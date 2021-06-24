3554a8d36199ef6880250204dd10b128bb19c3f1
Faisal Nadeem <faisal.nadeem@cityscapeone.com>
2021-06-24 22:29:32 +0500

    Firebase

--- components/Main.js
+++ components/Main.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {View,Text} from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import firebase from 'firebase'
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {fetchUser,fetchUserPosts} from '../redux/actions/index';
+import {fetchUser,fetchUserPosts,fetchUserFollowing} from '../redux/actions/index';
 
 import FeedScreen from './main/Feed';
 import ProfileScreen from './main/Profile';
+import SearchScreen from './main/Search';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -21,6 +23,7 @@ export class Main extends Component {
   componentDidMount(){
       this.props.fetchUser();
       this.props.fetchUserPosts();
+      this.props.fetchUserFollowing();
   }
 
   render() {
@@ -30,6 +33,13 @@ export class Main extends Component {
           options={{
             tabBarIcon:({color,size}) =>(
               <MaterialCommunityIcons name="home" color={color} size={size}/>
+            )
+          }}
+        />
+        <Tab.Screen name="Search" component={SearchScreen}  navigation={this.props.navigation}
+          options={{
+            tabBarIcon:({color,size}) =>(
+              <MaterialCommunityIcons name="magnify" color={color} size={size}/>
             )
           }}
         />
@@ -46,7 +56,13 @@ export class Main extends Component {
             )
           }}
         />
-        <Tab.Screen name="Profile" component={ProfileScreen} 
+        <Tab.Screen name="Profile" component={ProfileScreen}
+          listeners={({navigation}) =>({
+                tabPress: event =>{
+                    event.preventDefault();
+                      navigation.navigate("Profile", {uid: firebase.auth().currentUser.uid})
+                }
+          })}  
           options={{
             tabBarIcon:({color,size}) =>(
               <MaterialCommunityIcons name="account-circle" color={color} size={size}/>
@@ -62,5 +78,5 @@ const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser
 })
 
-const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser,fetchUserPosts},dispatch)
+const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser,fetchUserPosts,fetchUserFollowing},dispatch)
 export default connect(mapStateToProps,mapDispatchProps)(Main)
--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,30 +1,118 @@
-import React from 'react'
-import {StyleSheet,View,Text,Image,FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react'
+import {StyleSheet,View,Text,Image,FlatList,Button} from 'react-native';
 
+import firebase from 'firebase';
+require('firebase/firestore');
 import {connect} from 'react-redux';
 
 function Profile(props) {
-  const{ currentUser, posts} = props;
-  console.log(posts);
+  const [userPosts,setUserPosts] = useState([]);
+  const [user,setUser] = useState(null);
+  const[following,setFollowing] = useState(false);
+
+  useEffect(()=>{
+    const{ currentUser, posts} = props;
+
+    if(props.route.params.uid === firebase.auth().currentUser.uid){
+          setUser(currentUser)
+          setUserPosts(posts)
+    }
+    else{
+          firebase.firestore()
+            .collection("users")
+            .doc(props.route.params.uid)
+            .get()
+            .then((snapshot)=> {
+                if(snapshot.exists){
+                  setUser(snapshot.data());
+                }
+                else{
+                  console.log("Does not exist");
+                }
+              })
+
+          firebase.firestore()
+            .collection("posts")
+            .doc(props.route.params.uid)
+            .collection("userPosts")
+            .orderBy("creation","asc")
+            .get()
+            .then((snapshot)=> {
+                let posts = snapshot.docs.map(doc => {
+                const data = doc.data();
+                const id = doc.id;
+                    return {id,...data}
+                })
+                 setUserPosts(posts)
+              }
+      
+              )
+    }
+
+    if(props.following.indexOf(props.route.params.uid) > -1){
+        setFollowing(true);
+    }
+    else{
+        setFollowing(false);
+    }
+  },[props.route.params.uid,props.following])
+
+  const onFollow = () => {
+      firebase.firestore()
+      .collection("following")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userFollowing")
+      .doc(props.route.params.uid)
+      .set({})
+  }
+  const onUnfollow = () => {
+    firebase.firestore()
+    .collection("following")
+    .doc(firebase.auth().currentUser.uid)
+    .collection("userFollowing")
+    .doc(props.route.params.uid)
+    .delete({})
+  }
+  if(user === null){
+    return <View/>
+  }
   return (
     <View style={styles.container}>
       <View style={styles.containerInfo}>
-        <Text>{currentUser.name}</Text>
-        <Text>{currentUser.email}</Text>
+        <Text>{user.name}</Text>
+        <Text>{user.email}</Text>
+
+        {props.route.params.uid !== firebase.auth().currentUser.uid?(
+          <View>
+            {following?( 
+              <Button
+                title="Following"
+                onPress={()=> onUnfollow()}
+              />
+            ):
+            (
+              <Button
+              title="Follow"
+              onPress={()=> onFollow()}
+            />
+            )}
+          </View>
+        ):null}
       </View>
       <View style={styles.containerGallery}>
         <FlatList
           numColumns={3}
           horizontal={false}
-          data={posts}
+          data={userPosts}
           renderItem={({item})=>(
-            <View style={styles.containerImage}>
+            <View style={styles.containerImage} key={item.key}>
                 <Image
-                    style={styles.image}
-                    source={{uri:item.downloadURL}}
+                  source={{uri:item.downloadURL}}
+                  style={styles.image}
                 />
             </View>
           )}
+          keyExtractor={item => item.id}
         />
       </View>
     </View>
@@ -34,7 +122,6 @@ function Profile(props) {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    marginTop:40
   },
   containerInfo:{
     margin:20
@@ -43,16 +130,17 @@ const styles = StyleSheet.create({
     flex:1,
   },
   containerImage:{
-    flex:1/3
+    flex:1/3,
   },
   image:{
     flex:1,
-    aspectRatio:1/1
+    aspectRatio:1/1,
   }
 })
 const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser,
-  posts:  store.userState.posts
+  posts:  store.userState.posts,
+  following: store.userState.following
 })
 
 export default connect(mapStateToProps,null)(Profile);
